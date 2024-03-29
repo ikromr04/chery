95a37f63e45528018cf5173ccb8e565eacfd30ce
ikromr04 <ikromr04@gmail.com>
2024-03-29 13:39:51 +0500

    noindex, nofollow

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,8 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="keywords" content="">
-  <meta name="description" content="">
+  <meta name="robots" content="noindex,nofollow">
 
   @yield('styles')
 
